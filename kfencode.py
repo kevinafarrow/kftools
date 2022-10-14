e2266b374963fd8cd7d7a19d96a5ba838123f584
Kevin Farrow <github@kfarrow.com>
2022-10-14 10:37:27 -0700

    kfencode: interactive mode, kfpasswd: copy to clipboard

--- kfencode.py
+++ kfencode.py
@@ -4,12 +4,18 @@
 import os
 import click
 import secrets
+import pyperclip
 from sys import exit
 from output import message as m
 
 
 KEY = ''
 KEYLENGTH = 0
+key = None
+mode = None
+show = None
+file_out = None
+clipboard = None
 
 
 def encode_character(character):
@@ -32,32 +38,49 @@ def decode_message(message):
 
 
 @click.command()
-@click.option('--mode','-m',help="Decode a message",type=click.Choice(['encode','decode']),required=True)
-@click.option('--text','-t',help="Put a string in directly (for small messages)",type=click.STRING)
-@click.option('--input','-i',help="File containing message to encode/decode",type=click.STRING)
-@click.option('--output','-o',help="File to write output to",type=click.STRING)
-@click.option('--key','-k',help="Secret key from kfsecret.py",type=click.STRING,required=True)
+@click.option('--key','-k',help="Secret key from kfsecret.py",type=click.STRING)
+@click.option('--mode','-m',help="Decode a message",type=click.Choice(['encode','decode']))
+@click.option('--file_in','-i',help="File containing message to encode/decode",type=click.STRING)
+@click.option('--file_out','-o',help="File to write output to",type=click.STRING)
 @click.option('--show','-s',help="Print encoded message to stdout",is_flag=True)
+@click.option('--clipboard','-c',help="Copy text to clipboard.",is_flag=True)
 
-def main(mode, text, input, output, show, key):
+def main(mode, file_in, file_out, show, key, clipboard):
 
     global KEY
     global KEYLENGTH
 
-    m('Now opening the key file. This will take a moment for large keys...',status='attention')
+    if key == None:
+        key = ''
+        key = input('Please enter the location of the key: ')
+
+    m('Now opening the key file. This will take a moment for large keys...')
     with open(key, 'r') as f:
         KEY = f.read()
 
     KEYLENGTH = len(KEY)
     m(f"Key of length {KEYLENGTH} loaded")
 
-    if text and input:
-        m("The text flag and input flag cannot be used together.")
-        exit(1)
-
-    if input:
-        with open(input, 'r') as f:
+    if file_in:
+        with open(file_in, 'r') as f:
             text = f.read()
+    else:
+        m('Entering interactive mode')
+        m('Type /stop on its own line to signal completion.')
+        text = ''
+        while True:
+            line = input('[-]  > ')
+            if line == '/stop':
+                break
+            text += line
+
+    if mode == None:
+        show = None # This forces show back into interactive mode. 
+        clipboard = None # Ditto
+        mode = ''
+        valid = ['encode', 'decode']
+        while mode not in valid:
+            mode = input('[!]  Please enter a valid mode (encode or decode): ').lower()
 
     if mode == 'encode':
         processed_message = encode_message(text)
@@ -65,14 +88,40 @@ def main(mode, text, input, output, show, key):
     if mode == 'decode':
         processed_message = decode_message(text)
 
+    if show == None:
+        answer = ''
+        valid = {'y': True, 'n': False}
+        while answer not in valid.keys():
+            answer = input('[!]  Do you want to show results in stdout? (yes or no): ').lower()[0]
+        show = valid[answer]
+
     if show:
         m(processed_message)
+
+    if clipboard == None:
+        answer = ''
+        valid = {'y': True, 'n': False}
+        while answer not in valid.keys():
+            answer = input('[!]  Do you want to copy the results to the clipboard? (yes or no): ').lower()[0]
+        clipboard = valid[answer]
+
+    if clipboard:
+        pyperclip.copy(processed_message)
+
+    if file_out == None:
+        answer = ''
+        valid = {'y': True, 'n': False}
+        while answer not in valid:
+            answer = input('[!]  Do you want to save this to a file? (yes or no): ').lower()[0]
+        file_out = valid[answer]
+        if file_out:
+            file_out = input('[!]  Where would you like to put it?: ')
     
-    if output:
-        if os.path.exists(output):
+    if file_out:
+        if os.path.exists(file_out):
             m('That file already exists!', status='error')
             exit(1)
-        with open(output, 'w') as f:
+        with open(file_out, 'w') as f:
             f.write(processed_message)
 
 
